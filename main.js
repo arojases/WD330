a408d6681f69cf6d4e5fe269d0c0fc7a6e6717f9
Ariel <arielalejandrorojas@gmail.com>
2022-04-23 21:09:17 -0600

    final week1

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ const weeks = [
         labels: "Week 1 notes",
         url: "week1/index.html"
     },
-    {
+    /*{
         labels: "Week 2 notes",
         url: "week1/index.html"
     },
     {
         labels: "Week 3 notes",
         url: "week1/index.html"
-    }
+    }*/
 ];
 var list = weeks.map(function (link) {
     return ("<li><a href=" + link.url + ">" + link.labels + "</a></li>");
