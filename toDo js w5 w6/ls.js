a40acdc10a26c9db447cbebea896acf74f5571ef
Ariel <arielalejandrorojas@gmail.com>
2022-05-27 19:22:44 -0600

    implement todo list

--- toDo js w5 w6/ls.js
+++ toDo js w5 w6/ls.js
@@ -1,5 +1,15 @@
-let toDoItem = {
-    id: timestamp, 
-    content: string, 
-    complete: bool
-}
+/**
+ * LS: module for browser localStorage manipulation
+ */
+
+ const ls = {
+    // reads an item from localStorage and parses as JSON
+    read: (key) => {
+       return JSON.parse(localStorage.getItem(key));
+    },
+    // uses a JSON object to written as a string to localStorage
+    write: (key, data) => {
+       localStorage.setItem(key, JSON.stringify(data));
+    }
+  }
+  export default ls;
